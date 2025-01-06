9a1c7a5052856d05e0f2c3377e3fb7280adb76c9
Atharvapatil1800 <patilatharva1800@gmail.com>
2025-01-06 22:29:51 +0530

    do_while.java

--- do_while.java
+++ do_while.java
@@ -1,10 +1,10 @@
-public class do_while {
-        public static void main(String[] args) {
-            int i=1;
-            do {
-                System.out.println(i);
-                i++;
-            } while (i<=100);
+public class do_while {   // Define main class
+        public static void main(String[] args) {  // The main method where the program starts
+            int i=1;    // Decleration of integer i with value 1
+            do {  // Strt of do-while loop
+                System.out.println(i);  // Print thevalue of i
+                i++;   // Incremnt of i by 1
+            } while (i<=100);  // Continue the loop as i is less than or equal to 100
         }
 
 
