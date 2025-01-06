0565b38fa68c74ef27f8c7387d38754ab89b42be
Atharvapatil1800 <patilatharva1800@gmail.com>
2025-01-06 20:43:08 +0530

    First.java

--- First.java
+++ First.java
@@ -1,8 +1,8 @@
-import java.util.Scanner;
-class First {
-    public static void main(String[] args) {
-        double area, r;
-        Scanner sc = new Scanner(System.in);
+import java.util.Scanner;   // Import the scanner class to take input from user input
+class First {   // Define main class
+    public static void main(String[] args) {   // The main method where the program starts
+        double area, r;   // Declare two double variable 
+        Scanner sc = new Scanner(System.in);  // create a scanner object to take user input
         System.out.println("Enter radius");
         r = sc.nextDouble();
         area = 3.14159*r*r;
