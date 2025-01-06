d5b0e4cd24fe229b732a2f26f3cc088fb5ef6415
Atharvapatil1800 <patilatharva1800@gmail.com>
2025-01-06 22:15:21 +0530

    Break_statement.java

--- Break_statement.java
+++ Break_statement.java
@@ -1,12 +1,12 @@
-public class Break_statement {
-        public static void main(String[] args) {
-            int i=1;
-            while (i<=10){
-                System.out.println(i);
-                if (i==5){
-                    break;
+public class Break_statement {     // Defne main class
+        public static void main(String[] args) {   //The main metod where the program starts
+            int i=1;   // Dcleration of integer 'i' with value 1
+            while (i<=10){   // while loop to print number from 1 to 10
+                System.out.println(i);   // Print the current value of i
+                if (i==5){   // check if i equal to 5
+                    break;  // Exit the while loop
                 }
-                i++;
+                i++;  // Increment of i by 1
             }
         }
     }
